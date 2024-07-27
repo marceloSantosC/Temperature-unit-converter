0693c3b1ef4d606df2b1c3e59d5a23334bea93c7
Marcelo <marcelo17082000@gmail.com>
2024-07-27 17:42:48 -0300

    Add project files.

---
+++ src/UnitConverter/Models/Enumeration/SupportedLengthUnits.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitConverter.src.UnitConverter.Models.Enumeration
+{
+    public enum SupportedLengthUnits
+    {
+        Kilometer,
+        Meter,
+        Centimeter,
+        Millimetres,
+        Feet
+    }
+}
---
+++ src/UnitConverter/Models/Enumeration/SupportedTemperatureUnits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.src.UnitConverter.Models.Enumeration
+{
+
+    public enum SupportedTemperatureUnits
+    {
+        Celsius,
+        Fahrenheit,
+        Kelvin
+    }
+}
