cf8af5424db312f383799f070b9ab043712a49dd
Marcelo dos Santos Cardoso <marcelo17082000@gmail.com>
2024-07-28 12:34:14 -0300

    Created enums to represent Temperature units of measurement

---
+++ src/UnitConverter/Models/Enumeration/TemperatureConversions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.src.UnitConverter.Models.Enumeration
+{
+    public class TemperatureConversions
+    {
+
+        public static readonly TemperatureConversions KelvinToCelsius = new(TemperatureUnits.Kelvin, TemperatureUnits.Celsius);
+        public static readonly TemperatureConversions KelvinToFahrenheit = new(TemperatureUnits.Kelvin, TemperatureUnits.Fahrenheit);
+
+        public static readonly TemperatureConversions CelsiusToKelvin = new(TemperatureUnits.Celsius, TemperatureUnits.Kelvin);
+        public static readonly TemperatureConversions CelsiusToFahrenheit = new(TemperatureUnits.Celsius, TemperatureUnits.Fahrenheit);
+
+        public static readonly TemperatureConversions FahrenheitToCelsius = new(TemperatureUnits.Fahrenheit, TemperatureUnits.Celsius);
+        public static readonly TemperatureConversions FahrenheitToKelvin = new(TemperatureUnits.Fahrenheit, TemperatureUnits.Kelvin);
+
+        public TemperatureUnits From { get; }
+        public TemperatureUnits To { get; }
+
+        private TemperatureConversions(TemperatureUnits firstUnit, TemperatureUnits secondUnit)
+        {
+            From = firstUnit;
+            To = secondUnit;   
+        }
+
+
+        public bool CanConvert(TemperatureUnits from, TemperatureUnits to)
+        {
+            return From.Equals(from) && To.Equals(to);
+        }
+
+    }
+}
---
+++ src/UnitConverter/Models/Enumeration/TemperatureUnits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.src.UnitConverter.Models.Enumeration
+{
+
+    public enum TemperatureUnits
+    {
+        [Description("Graus Celsius")]
+        Celsius = 1,
+
+        [Description("Fahrenheit")]
+        Fahrenheit = 2,
+
+        [Description("Kelvin")]
+        Kelvin = 3
+    }
+
+}
